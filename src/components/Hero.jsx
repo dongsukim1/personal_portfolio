db48f244dcf2b27c466a2410da8cc9e607ccd619
dongsukim <dongsukim@berkeley.edu>
2025-05-26 03:14:02 -0700

    readme placeholder

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -107,7 +107,7 @@ const Hero = ({ name }) => {
         <Row className="align-items-center text-center">
           <Col>
             <h1 className="mb-3 display-3 title">
-              {name === null ? "null" : name}
+              {name === null ? "Dongsu Kim" : name}
             </h1>
             <div className="d-flex align-items-center justify-content-center">
               <SocialLinks />
